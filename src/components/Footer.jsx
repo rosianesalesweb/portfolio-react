87f2c74aae55004bb11753bc4c53455d8b7f58bc
Rosiane Sales <dossantosrosiane7@gmail.com>
2026-04-17 23:48:56 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <h1>2026 Desenvolvido por Rosiane Sales | dossantosrosiane7@gmail.com</h1> 
+  return (
+    <footer>
+      <p>026 Desenvolvido por Rosiane Sales | dossantosrosiane7@gmail.com</p>
+    </footer>
+  );
 }
